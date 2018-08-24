46327b2c2cf39c996bbe71855104567df553404a
Christopher Mosqueda <christophermosqueda@Christophers-MacBook-Pro.local>
2018-08-24 10:49:41 -0500

    Something went wrong

--- seeder.sql
+++ seeder.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE IF NOT EXISTS adlister_db;
-
-USE tables_lessons_db;
+USE adlister_db;
 
 DROP TABLE IF EXISTS users;
 
@@ -18,14 +16,13 @@ CREATE TABLE ads(
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   user_id INT UNSIGNED NOT NULL,
   title VARCHAR(100) NOT NULL,
-  descriptoion TEXT NOT NULL,
+  description TEXT NOT NULL,
   PRIMARY KEY(id),
-  FOREIGN KEY (user_id) REFERENCES users (id)
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 SHOW TABLES;
 DESCRIBE users;
 DESCRIBE ads;
 
-INSERT into users(id, username, email, password) values('1', 'admin', 'email', 'password')
 SELECT * from ads
--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -7,9 +7,12 @@ import com.mysql.cj.jdbc.Driver;
 public class MySQLAdsDao implements Ads{
     private Connection connection;
 
+    // make connection with config class
     public MySQLAdsDao(Config config) {
         try {
+            // need to import correct driver pckg
             DriverManager.registerDriver(new Driver());
+            // makes connection w/ Config.java class
             connection = DriverManager.getConnection(
                     config.getUrl(),
                     config.getUser(),
@@ -24,8 +27,12 @@ public class MySQLAdsDao implements Ads{
         try {
             String selectQry = "SELECT * FROM ads";
             Statement stmt = connection.createStatement();
+            // stmt executes the sql (query)
             ResultSet rs = stmt.executeQuery(selectQry);
+            // make a new list to add to and return
             List<Ad> newList = new ArrayList<>();
+                // can make into another method -
+                // private List<Ad> createList(ResultSet rs){...}
             while(rs.next()) {
                 newList.add(new Ad(
                         rs.getLong("id"),
@@ -37,21 +44,40 @@ public class MySQLAdsDao implements Ads{
             return newList;
         } catch (SQLException e) {
             e.printStackTrace();
+            // will halt execution of everything else if there is an error
             throw new RuntimeException("Problem buidling");
         }
     }
 
+    // can make a main method to test connection before anything else
+//    psv main(){
+//        Config config = new Config();
+//        MySQLAdsDao adsDao = new MySQLAdsDao(config);
+//        List<Ad> ads = adsDao.all();
+//        //getter methods
+//        String testSHow = ads.get(0).getTitle(); //- will show the first title in db
+//        System.out.println(testSHow);
+//    }
+
     @Override
+    // giving insert method an Ad Obj (ad)
     public long insert(Ad ad) {
         try {
-            String query = "INSERT INTO ads(user_id, title, description) VALUES ('"+ad.getUserId()+"', '"+ad.getTitle()+"','"+ ad.getDescription()+"');";
             Statement stmt = connection.createStatement();
+            // String query = String.format("Insert into ...() values(%d, '%s', '%s')",
+            // ad.getUserId(),
+            // ad.getTitle(),
+            // ad.getDescription());
+            String query = "INSERT INTO ads(user_id, title, description) VALUES ('"+ad.getUserId()+"', '"+ad.getTitle()+"','"+ ad.getDescription()+"');";
+                                    //below gets the last inserted keys
             stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next();
-            return rs.getLong(1);
+            //to get result set have to run a separate method, .getGenratedKeys()
+            ResultSet genKeys = stmt.getGeneratedKeys();
+            genKeys.next();
+            return genKeys.getLong(1);
         } catch (SQLException e) {
             e.printStackTrace();
+            // will halt execution of everything else if there is an error
             throw new RuntimeException("Problem buidling");
         }
     }
